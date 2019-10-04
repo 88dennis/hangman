02b2dca15a33fe4f30a2baee4f920be5978387d4
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2019-10-04 10:56:26 -0400

    up

--- scripts/script.js
+++ scripts/script.js
@@ -32,7 +32,6 @@ document.getElementById("formForGuess").addEventListener("submit", getGuess);
 function getWord(event) {
     event.preventDefault();
     word = wordInput.value.toUpperCase();
-    
     guessWrapper.style.display ="block"
     guessInput.focus()
     youWin.style.display = "none"
@@ -41,13 +40,10 @@ function getWord(event) {
     wrongCont.innerHTML = wrongText;
     revealedLetters = new Array(word.length);
     revealedLetters.fill('');
-    console.log(revealedLetters, "blank Array")
     maxStrike = word.length * 2;
     strikeLetters = new Array()
-    console.log("strike letters", strikeLetters)
     strike = 0;
 
-    console.log(maxStrike, "this is the maximum allowed strike")
     text = "<div class = 'spanContainer'>";
     for (let i = 0; i < revealedLetters.length; i++) {
         text += "<span class = 'letterSpan'>" + revealedLetters[i] + "</span>";
@@ -66,9 +62,8 @@ function getGuess(event) {
         if (!word.includes(guessedLetter)) {
             strike++
                 strikeLetters.push(guessedLetter)
-                console.log(strikeLetters)
                 let num = strikeLetters.length;
-                console.log(num, "++++++++++++++++++++++++++++")
+
                 drawHangMan(num)
 
                 wrongText = "<div class = 'wrongWrap'><span>Guesses: </span>";
@@ -80,10 +75,7 @@ function getGuess(event) {
             }
             for (let i = 0; i < revealedLetters.length; i++) {
                 if (guessedLetter === word[i]) {
-                    console.log(word[i], "CORRECT LETTER")
                     revealedLetters[i] = guessedLetter;
-
-                    console.log(revealedLetters, "CORRECT SO FAR")
                     spans[i].textContent = guessedLetter
                     spans[i].style.background ="none"
                     spans[i].style.borderStyle ="solid"
@@ -96,13 +88,11 @@ function getGuess(event) {
         }
 
         if (!revealedLetters.includes("")) {
-            console.log("PANALO")
             youWin.style.display = "block"
             strike = 0;
             maxStrike = 0;
             guessWrapper.style.display ="none"
             imgElem.src = imgSrc7;
-
         }
     }
     guessInput.value = ""
@@ -111,157 +101,24 @@ function getGuess(event) {
 
 function drawHangMan(nums) {
     if (nums === 1 && nums <= maxStrike * .2) {
-        console.log("hellos")
         imgElem.src = imgSrc1;
     }
     if (nums > maxStrike * .2 && nums <= maxStrike * .4) {
-        console.log("gasgaf")
         imgElem.src = imgSrc2;
     }
     if (nums > maxStrike * .4 && nums <= maxStrike * .5) {
-        console.log("asdasl")
         imgElem.src = imgSrc3;
     }
     if (nums > maxStrike * .5 && nums <= maxStrike * .6) {
-        console.log("asdasdasasdadsa");
         imgElem.src = imgSrc4;
     }
     if (nums > maxStrike * .6 && nums <= maxStrike * .8) {
-        console.log("FFFFFFFFFFF")
         imgElem.src = imgSrc5;
     }
     if (nums === maxStrike) {
-        console.log("SA WAKAS")
         imgElem.src = imgSrc6;
         guessWrapper.style.display ="none"
         youLoose.style.display = "block"
         
     }
 }
-
-
-
-
-
-
-// 
-// let wordArr = new Array(word.length);
-// let wordArrLetter =""
-
-// let guessedArr = []
-
-
-
-// let totalHits = 0;
-// let correctGuess = 0;
-// let wrongGuess = 0;
-// let totalCorrect = 0;
-
-
-// function getWord(event) {
-
-
-//         console.log(word);
-//         console.log(wordArr)
-//         wordArrLetter = wordArr.fill("--")
-//         // wordArrNew = wordArr[0]
-//         console.log(wordArr)
-
-//     }
-
-// }
-
-
-//     if (wordArrNew) {
-
-//         strike = strike + 1;
-//         console.log("STRIKE No " + strike)
-//         for (let i = 0; i < wordArrNew.length; i++) {
-//             if (guessedLetter === wordArrNew[i] ) {
-
-//                 if (guessedArr[i] !==wordArrNew[i] && !guessedArr.includes(guessedLetter) ){
-//                     guessedArr.splice(wordArr.indexOf(wordArrNew[i]), 0, wordArrNew[i])
-//                     console.log(wordArrNew)
-//                     console.log(guessedArr, "GUESSED ARRAY")
-//                 }
-//             }
-
-//             // {
-//             //     
-//             //     
-//             //     console.log(guessedLetter, "guessed letter")
-//             //     // if(guessedArr[i] !==wordArrNew[i]){
-//             //     //     correctGuess = correctGuess + 1;
-//             //     //     console.log(correctGuess, "CORRECT GUESS")
-//             //     // }
-//             // }
-//         }
-
-//     }
-
-//     // for (let j = 0; j < spans.length; j++) {
-//     //     let letter = (spans[j].textContent)
-//     //     boxArr.push(letter)
-//     //     console.log(letter)
-//     //     console.log(boxArr)
-//     //     // .join("").push(letter)
-//     // }
-// }
-// // console.log(totalStrike)
-// // console.log(strike)
-
-// // function addStrikes(hit, correct) {
-// // totalHits = totalHits + hit;
-// // // totalCorrect = totalCorrect + correct
-// // // wrongGuess = totalHits - correctGuess
-// // console.log(totalHits, "STRIKETOTAL")
-// // // console.log(totalCorrect, "totalCorrect")
-// // }
-
-
-
-
-
-// // for (let i = 0; i < word.length; i++) {
-
-// //     console.log(word.lenght, 'length')
-// //                 if (wordArr[0].length === guessedArr.length && word[i] === guessedArr[i]) {
-// //                     console.log("you win")
-// //                     wordArr = [];
-// //                     guessedArr = [];
-// //                     // console.log(wordArr)
-// //                     // console.log(guessedArr)
-// //                 }
-
-
-// // if(guessedLetter) {
-// //     addStrikes(strike, correctGuess)
-// //     // console.log(strike)
-// //    '
-// // }
-
-// // }
-
-
-
-//             //      if (word[i] !== guessedArr[i]){
-//             //                 guessedArr.push(guessedLetter)
-
-//             //                 spans[i].textContent = word[i]
-//             //                     correctGuess = 1;
-//             //                     console.log("=========================")
-//             //                     console.log(correctGuess, "CORRECT GUESS")
-//             //         console.log(guessedArr)
-//             //     // }
-//             // } else if(word[i] !== guessedLetter){
-//             //     // console.log(word[i])
-//             //     console.log(guessedLetter)
-//             //     strike = 1;
-//             // }
-//             // if (spans.textContent === wordArr[0].join("")) {
-//             //     console.log("you win")
-//             // }
-//             // console.log(wordArr[0].length)
-//             // console.log(guessedArr.length)
-
-
